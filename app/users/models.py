61a701eb0520779ea73c2811f5078de336f8baa8
yoo-sh-96 <tmdguq90051@gmail.com>
2025-07-02 13:46:43 +0900

    create custom user model

--- app/users/models.py
+++ app/users/models.py
@@ -1,5 +1,49 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser, # 모델이 커스텀유저로 상속을 받게끔
+    PermissionsMixin, # 일반유저와 슈퍼유저를 구분하기위해
+    BaseUserManager
+)
 from django.db import models
 
+class UserManager(BaseUserManager):
+    # 일반 유저 생성 함수
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("Please enter an eamil address")
+        
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+
+        return user
+    
+    # 슈퍼 유저 생성
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save()
+
+        return user
+    
+# - email
+# - password
+# - nickname
+# - is_business
 # Create your models here.
-class User():
-    pass
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.CharField(max_length=255, unique=True)
+    nickname = models.CharField(max_length=255)
+    is_business = models.BooleanField(default=False)
+
+    # Permissions Mixin : 유저의 권한 관리
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self): # 클래스안의 함수이기에 self를 받는다
+        return f'email: {self.email}, nickname: {self.nickname}'
--- app/users/tests.py
+++ app/users/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+# TDD : Test Drvien Development (테스트 주도 개발)
+class UserTestCase(TestCase):
+
+    # 일반유저 생성 테스트 함수
+    def test_create_user(self):
+        email = 'seunghyeop@gamil.com'
+        password = '1q2w3e4r'
+
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        # 유저가 정상적으로 잘 만들어졌는지?
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password)) #self.assertEqual(user.check_password(password), True)
+        self.assertFalse(user.is_superuser) #self.assertEqual(user.is_superuser, False)
+    
+    # 슈퍼유저 생성 테스트 함수
+    def test_create_superuser(self):
+        email = 'you_super@gmail.com'
+        password = '1q2w3e4r'
+
+        user = get_user_model().objects.create_superuser(
+            email = email,
+            password=password
+        )
+
+        # 슈펴유저
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+        # docker-compose run --rm app sh -c 'python manage.py test users'
+        # docker-compose run --rm app sh -c 'python manage.py migrate'   
+        # docker-compose run --rm app sh -c 'python manage.py makemigrations' 
